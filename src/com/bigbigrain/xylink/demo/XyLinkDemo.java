9fb17ef4da2f20e9dfea1f8c0383f7750bb39870
B1gB1gRAin <491036107@qq.com>
2017-10-17 15:54:48 +0800

    调整包结构  增加小鱼link包

---
+++ src/com/bigbigrain/xylink/demo/XyLinkDemo.java
@@ -0,0 +1,12 @@
+package com.bigbigrain.xylink.demo;
+
+/**
+ * @ClassName: XyLinkDemo
+ * @Description: 小鱼link使用说明
+ * @author B1gB1gRAin
+ * @date 2017年10月17日 下午3:51:47
+ *
+ */
+public class XyLinkDemo {
+	
+}
